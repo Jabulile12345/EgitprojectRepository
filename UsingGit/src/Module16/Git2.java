5a7125751d647d2a6de7434d63e3566369d60a53
Mbusi <sitolejp7@gmail.com>
2016-04-15 15:41:48 +0200

    Adding Output To My Code

--- UsingGit/src/Module16/Git2.java
+++ UsingGit/src/Module16/Git2.java
@@ -4,5 +4,6 @@ public class Git2 {
 
 	public static void displaygreetings(){
 		System.out.println("Welcome to Our Database");
+		System.out.println("You can Perform Any Task From The  List Below ");
 		}
 		}
